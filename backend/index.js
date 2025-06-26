13114c37f506cc137977c0cf3601794eda87695d
erick <erickbranquinhomachado@gmail.com>
2025-06-26 12:39:38 -0300

    rota de criacao de evento

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,38 @@
+const express = require("express");
 const dotenv = require("dotenv");
+const { MongoClient } = require("mongodb");
+const eventRoutes = require("./routes/eventRoutes");
+const userRoutes = require("./routes/userRoutes")
 dotenv.config();
 
-const express = require("express");
 const app = express();
-const port = process.env.PORT;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Ola mundo! Como isso funciona?");
-});
+const port = process.env.PORT || 3000;
+const connectionString = process.env.ATLAS_URI;
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+const client = new MongoClient(connectionString);
+
+async function main() {
+    try {
+        await client.connect();
+        const db = client.db("kommunity_db");
+        console.log("Conectado com sucesso ao MongoDB");
+
+        app.get("/", (req, res) => {
+            res.send("Olá mundo!");
+        });
+
+        app.use("/events", eventRoutes(db));
+
+        app.use("/users", userRoutes(db));
+
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        });
+    } catch (e) {
+        console.error("Erro de conexão com o MongoDB:", e);
+    }
+}
+
+main();
