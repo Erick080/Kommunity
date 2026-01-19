da1e150697fd3aba173cca5aed923a6a504f7977
Erick <erickbranquinhomachado@gmail.com>
2026-01-19 14:52:30 -0300

    refatorando para spring boot

---
+++ docs/Instrucoes.md
@@ -0,0 +1,3 @@
+Para rodar localmente:
+-- Iniciar primeiro o BD: ``docker compose --env-file .env.local up -d postgres``
+-- Depois iniciar o Spring Boot: ``./mvnw spring-boot:run``
---
+++ src/test/java/koala/kommunity/KommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package koala.kommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
